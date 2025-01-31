bbdc92d3ba17a9cf6c62d0fef0bf504689a79638
FFilins <felipeclayton007@gmail.com>
2025-01-31 14:29:14 -0300

    dashboard page

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::middleware([AdminMiddleware::class])->group(function () {
     Route::get('/profile', function () {
         // ...
     })->withoutMiddleware([AdminMiddleware::class]);
-    Route::get('/admin/dashboard' , Dashboard::class)->name('admin.dashboard');
+    // Route::get('/admin/dashboard' , Dashboard::class)->name('admin.dashboard');
 
     // Route::get('/history', function(){
     //     return 'history';
