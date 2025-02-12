c529feb258cc11612f850b34bf2ae6bf8c3e8a2c
AbilioN <netobalby@gmail.com>
2025-02-12 10:33:06 -0300

    app

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ciano</title>
-    {{-- <link rel="stylesheet" href="{{ URL::asset('build/assets/app-ea88faee.css') }}"> --}}
+    <link rel="stylesheet" href="{{ URL::asset('build/assets/app-CKhGOyLN.css') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
