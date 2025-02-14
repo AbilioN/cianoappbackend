4b1997519daab10f4fff2a6dcb03460c6482ee1e
AbilioN <netobalby@gmail.com>
2025-02-14 08:56:36 -0300

    fix url

--- database/seeders/DefaultNotificationsSeeder.php
+++ database/seeders/DefaultNotificationsSeeder.php
@@ -457,7 +457,7 @@ class DefaultNotificationsSeeder extends Seeder
             'slug' => slugify('WATER BIO-BACT S'),
             'code' => 'COM560019',
             'description' => 'Water bio-bact S',
-            'image_url' => 'http://app.ciano.pt/images/bio-bact.png',
+            'image_url' => 'http://app.ciano.pt/images/bio_bact.png',
         ]);
 
 
