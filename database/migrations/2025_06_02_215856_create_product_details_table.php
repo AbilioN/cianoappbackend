eb29b1568c2490d58cf15e7c1406e7d8c69a7360
AbilioN <netobalby@gmail.com>
2025-06-02 19:26:17 -0300

    product detail

---
+++ database/migrations/2025_06_02_215856_create_product_details_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->enum('type', [
+                'large_image',
+                'title',
+                'list',
+                'ordered_list',
+                'divider',
+                'small_image',
+                'medium_text',
+                'small_text',
+                'title_left'
+            ]);
+            $table->json('content');
+            $table->integer('order')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_details');
+    }
+};
